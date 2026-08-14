a53c09e59acd8f5294849c43b21813045e96406a
wuzhuohua168 <wuzhuohua168@users.noreply.github.com>
2026-08-14 12:58:29 +0800

    trigger workflow with new sources

--- generator.py
+++ generator.py
@@ -30,7 +30,7 @@ from urllib.parse import quote
 import requests
 import yaml
 
-VERSION = "v1.2"
+VERSION = "v1.2.1"
 CLASH_OUTPUT = Path("output/clash.yaml")
 ROCKET_OUTPUT = Path("output/rocket.txt")
 V2RAY_OUTPUT = Path("output/v2ray.txt")
